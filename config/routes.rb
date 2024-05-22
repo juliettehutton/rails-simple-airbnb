68fc8355b0c8f07ce8373ed0958c660d8bd986f7
Juliette Hutton <juliettehutton2@gmail.com>
2024-05-22 17:02:14 -0400

    created CRUD actions

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :flats
+  get "flats", to: "flats#index"
+
+  get "flats/new", to: "flats#new", as: :new_flat
+
+  get "flats/:id", to: "flats#show", as: :flat
+
+  post "flats", to: "flats#create"
+
+  get "flats/:id/edit", to: "flats#edit", as: :edit_flat
+
+  patch "flats/:id", to: "flats#update"
+
+  delete "flats/:id", to: "flats#destroy"
 end
